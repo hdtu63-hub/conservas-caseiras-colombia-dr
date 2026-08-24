601234122f53b1ce3d7ec4bdcdedd778dcd9b1e8
Jackson Liebmann <jacksonliebmann@MacBook-Air-de-Jackson.local>
2026-08-24 20:19:10 -0300

    Perf: remover bloqueio de renderização do CSS e Scripts para melhor performance no PageSpeed

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,23 @@
 import type { Metadata } from "next";
+import { DM_Sans, Fraunces } from "next/font/google";
+import Script from "next/script";
 import "./globals.css";
 import UtmPersistence from "./utm-persistence";
+
+const dmSans = DM_Sans({
+  subsets: ["latin"],
+  weight: ["300", "400", "500", "600"],
+  variable: "--font-dm-sans",
+  display: "swap",
+});
+
+const fraunces = Fraunces({
+  subsets: ["latin"],
+  weight: ["300", "400"],
+  style: ["normal", "italic"],
+  variable: "--font-fraunces",
+  display: "swap",
+});
 
 export const metadata: Metadata = {
   title: "Conservas Caseras · 100+ Recetas Rentables",
@@ -15,8 +32,8 @@ export const metadata: Metadata = {
 
 export default function RootLayout({ children }: Readonly<{ children: React.ReactNode }>) {
   return <html lang="es-419">
-    <head>
-      <script dangerouslySetInnerHTML={{ __html: `
+    <body className={`${dmSans.variable} ${fraunces.variable}`}>
+      <Script id="fb-pixel" strategy="afterInteractive" dangerouslySetInnerHTML={{ __html: `
         !function(f,b,e,v,n,t,s){if(f.fbq)return;n=f.fbq=function(){n.callMethod?
         n.callMethod.apply(n,arguments):n.queue.push(arguments)};if(!f._fbq)f._fbq=n;
         n.push=n;n.loaded=!0;n.version='2.0';n.queue=[];t=b.createElement(e);t.async=!0;
@@ -31,9 +48,8 @@ export default function RootLayout({ children }: Readonly<{ children: React.Reac
           content_type: 'product'
         });
       ` }} />
-      <script src="https://cdn.utmify.com.br/scripts/utms/latest.js" async defer data-utmify-prevent-subids="" />
-    </head>
-    <body>
+      <Script src="https://cdn.utmify.com.br/scripts/utms/latest.js" strategy="lazyOnload" data-utmify-prevent-subids="" />
+      
       <noscript><img height="1" width="1" style={{ display: "none" }} src="https://www.facebook.com/tr?id=1832021307783603&ev=PageView&noscript=1" alt="" /></noscript>
       <UtmPersistence />
       {children}
